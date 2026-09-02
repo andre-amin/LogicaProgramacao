aedab4476a1afe26ee180f52e12df8915a24616e
André Amin <aluizamin@gmail.com>
2026-09-02 16:08:13 -0300

    Add beeCrowd1074, 1075, 1079 projects and solutions
    Added three new projects targeting .NET 10.0: beeCrowd1074, beeCrowd1075, and beeCrowd1079. Implemented solutions for beeCrowd1074 (integer classification) and beeCrowd1075 (print numbers with remainder 2). beeCrowd1079 project created with empty Program.cs.

---
+++ beeCrowd1074/Program.cs
@@ -0,0 +1,27 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < N; i++)
+{
+    int X = int.Parse(Console.ReadLine());
+    
+    if (X % 2 == 0 && X < 0)
+    {
+        Console.WriteLine("EVEN NEGATIVE");
+    }
+    else if (X % 2 != 0 && X < 0)
+    {
+        Console.WriteLine("ODD NEGATIVE");
+    }
+    else if (X % 2 == 0 && X > 0)
+    {
+        Console.WriteLine("EVEN POSITIVE");
+    }
+    else if (X % 2 != 0 && X > 0)
+    {
+        Console.WriteLine("ODD POSITIVE");
+    }
+    else if (X == 0)
+    {
+        Console.WriteLine("NULL");
+    }
+}
