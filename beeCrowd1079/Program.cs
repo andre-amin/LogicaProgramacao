1d9ba38a40194bfb145d9c9817677e79c4f3bea6
André Amin <aluizamin@gmail.com>
2026-09-15 14:54:40 -0300

    Add beeCrowd1079, 1080 & 1094 projects with solutions

--- beeCrowd1079/Program.cs
+++ beeCrowd1079/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System.Globalization;
+
+int N = int.Parse(Console.ReadLine());
+
+double X, Y, Z, media;
+
+for(int i = 0; i < N; i++)
+{
+    string[] vet = Console.ReadLine().Split(' ');
+    
+    
+    X = double.Parse(vet[0], CultureInfo.InvariantCulture);
+    Y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+    Z = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+    media = ((X * 2) + (Y * 3) + (Z * 5)) / 10;
+    Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+}
---
+++ beeCrowd1080/Program.cs
@@ -0,0 +1,19 @@
+﻿int num, pos, N;
+num = 0;
+pos = 0;
+
+for (int i = 1; i <= 100; i++)
+{
+    
+    N = int.Parse(Console.ReadLine());
+
+    if (N > num)
+    {
+        num = N;
+        pos = i;
+    }
+
+
+}
+Console.WriteLine(num.ToString());
+Console.WriteLine(pos.ToString());
