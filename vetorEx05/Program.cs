ebf536a580917076f5d6f4534818e257c8185889
André Amin <aluizamin@gmail.com>
2026-09-21 18:36:55 -0300

    Add vetorEx05 project to calculate average of evens
    Added vetorEx05 to solution and implemented Program.cs to:
    - Read N and N double values into an array
    - Calculate and print the average of even numbers with one decimal using invariant culture

---
+++ vetorEx05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+int N, pares;
+double soma, media;
+soma = 0.0;
+pares = 0;
+N = int.Parse(Console.ReadLine());
+
+double[] A = new double[N];
+
+string[] S = Console.ReadLine().Split(' ');
+
+for(int i = 0; i < N; i++)
+{
+    A[i] = double.Parse(S[i], CultureInfo.InvariantCulture);
+}
+
+foreach(double x in A)
+{
+    if(x % 2 == 0)
+    {
+        soma += x;
+        pares++;
+    }
+}
+media = soma / pares;
+Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
